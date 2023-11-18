268635c3590c3b0e092d666a936404dcb3571933
Ryan Atkinson <mail@ryanatkn.com>
2023-11-17 17:28:12 -0700

    install @changesets/cli

--- setup.sh
+++ setup.sh
@@ -111,7 +111,7 @@ else
   fnm use v20
   fnm default v20
   npm i -g npm
-  npm i -g @grogarden/gro
+  npm i -g @grogarden/gro @changesets/cli
   mkdir -p "$FISH_CONFIG_DIR"
   copy "$REPO_DIR/fish/settings.fish" "$FISH_CONFIG_DIR"
 fi
